50c2830889505b2170d67d5c460dcf4210cf3e4f
vedikathote <vedikathote@gmail.com>
2025-04-14 15:37:18 +0530

    made calculator

--- script.js
+++ script.js
@@ -1,48 +1,65 @@
-let output = document.getElementById("output");
-let buttons = document.getElementsByTagName("th");
-let equalto = document.getElementById("equal_to");
-let clear = document.getElementById("clear");
-let moon = document.getElementById("moon");
-let sun = document.getElementById("sun");
-let calScreen = document.querySelector(".cal_screen");
+let op=document.querySelectorAll(".op");
+let num=document.querySelectorAll(".num");
+let eql=document.getElementById("eql");
+let cls=document.getElementById("cls");
+let cut =document.getElementById("cut");
+let input="";
+let screen=document.getElementById("screen");
+op.forEach((sign)=>{
+     sign.addEventListener("click",()=>{
+        let lastChar = input[input.length - 1];
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("mousedown", function () {
-    buttons[i].style.backgroundColor = "#144c24";
-    buttons[i].style.color = "white";
-  });
+        
+        if (input === "" || ["+", "-", "*", "/"].includes(lastChar)) {
+            return;
+        }
+        input=input+sign.textContent;
+        screen.innerHTML=input;
+        screen.scrollLeft = screen.scrollWidth;
+        screen.classList.add("screen");
 
-  buttons[i].addEventListener("mouseup", function () {
-    buttons[i].style.backgroundColor = "";
-    buttons[i].style.color = "";
-  });
+    
+        
+     })
+     
+    })
+    
+    num.forEach((val)=>{
+        val.addEventListener("click",()=>{
+            
+            input=input+val.textContent;
+          
+            screen.innerHTML = input;
+            screen.scrollLeft = screen.scrollWidth;
+            screen.classList.add("screen");
+           
 
-  buttons[i].addEventListener("click", function () {
-    if (buttons[i] !== equalto && buttons[i] !== clear) {
-      output.innerHTML += buttons[i].textContent.trim();
-      output.style.fontWeight = "bold";
-      output.style.fontSize = "20px";
-    }
-  });
-}
+        })
+                                                                 
+      
+    
+})
 
-equalto.addEventListener("click", function () {
-  output.innerHTML = eval(output.innerHTML);
-});
+eql.addEventListener("click",()=>{
+    let ans=eval(input);
+    screen.innerHTML=" ";
+    screen.innerHTML=ans;
+    input = ans.toString(); 
+    
 
-clear.addEventListener("click", function () {
-  output.innerHTML = "";
-});
 
-moon.addEventListener("click", function () {
-  document.body.style.backgroundColor = "black";
-  calScreen.style.backgroundColor = "rgba(150, 149, 149,0.3)";
-  moon.style.display = "none";
-  sun.style.display = "block";
-});
-sun.addEventListener("click", function () {
-  document.body.style.backgroundColor = "#51A39D";
-  calScreen.style.backgroundColor = "rgba(255,255,255,0.8)";
-  moon.style.display = "block";
-  sun.style.display = "none";
-});
+})
+
+cls.addEventListener("click",()=>{
+   screen.innerHTML="";
+   input="";
+
+
+})
+
+
+cut.addEventListener("click",()=>{
+  input=  input.slice(0,-1);
+  screen.innerHTML=input;
+    
+})
